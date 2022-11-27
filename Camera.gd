d88d534f6c325d472c07f79a1bfb36e64dfe3966
TimofeyYeremiiev <TenebreaTrimer@gmail.com>
2022-11-27 23:12:01 +0100

    Linux done!

--- Camera.gd
+++ Camera.gd
@@ -1,39 +1,120 @@
 
 extends Camera
 
-export var state = "none"
+export var state = "norm"
 
 export var texture_subShieldZayavka : Material
 export var texture_subShield1 : Material
 export var texture_subShield2 : Material
 export var texture_subShield3 : Material
+
+export var texture_linuxInstall : Material
+var flag = false
+
+
 export var texture_camCorridor4 : Material
 export var texture_camRoom1 : Material
 
 
-func _process(delta):
+
+export var mon1 : NodePath
+export var mon2 : NodePath
+export var mon3 : NodePath
+export var mon4 : NodePath
+export var mon5 : NodePath
+export var mon6 : NodePath
+export var mon7 : NodePath
+export var mon8 : NodePath
+
+func _ready():
+	get_node(mon1).set_surface_material(0, texture_camRoom1)
+	get_node(mon2).set_surface_material(0, texture_subShield1)
+	get_node(mon3).set_surface_material(0, texture_subShield2)
+	get_node(mon4).set_surface_material(0, texture_subShield1)
+	get_node(mon5).set_surface_material(0, texture_subShield3)
+	get_node(mon6).set_surface_material(0, texture_subShield3)
+	get_node(mon7).set_surface_material(0, texture_subShieldZayavka)
+	get_node(mon8).set_surface_material(0, texture_camCorridor4)
+	pass
+
+func _process(_delta):
+	#Перемещения
 	if Input.is_action_pressed("game_watchDoor"):
+		state = "door"
 		$AnimationTree.set("parameters/conditions/wathDoor", true)
 		$AnimationTree.set("parameters/conditions/to_norm", false)
 	else:
 		$AnimationTree.set("parameters/conditions/wathDoor", false)
 		
 		if Input.is_action_pressed("game_zoomIn1"):
+			state = "cams1"
 			$AnimationTree.set("parameters/conditions/to_norm", false)
 			$AnimationTree.set("parameters/conditions/ZoomIn", true)
 		else:
 			$AnimationTree.set("parameters/conditions/ZoomIn", false)
 			
 			if Input.is_action_pressed("game_zoomIn2"):
+				state = "cams2"
 				$AnimationTree.set("parameters/conditions/to_norm", false)
 				$AnimationTree.set("parameters/conditions/ZoomIn2", true)
 			else:
 				$AnimationTree.set("parameters/conditions/ZoomIn2", false)
 				
 				if Input.is_action_pressed("game_watchBack"):
+					state = "back"
 					$AnimationTree.set("parameters/conditions/to_norm", false)
 					$AnimationTree.set("parameters/conditions/Back", true)
 				else:
+					state = "none"
 					$AnimationTree.set("parameters/conditions/Back", false)
 					$AnimationTree.set("parameters/conditions/to_norm", true)
+	#Гуишка музикбокса
+	$"../MusixBox/TimeBar".value = $"../MusixBox/AudioStreamPlayer3D".get_playback_position()
+	if $"../MusixBox/TimeBar".value > 180:
+		if flag == true:
+			return
+		flag = true
+		$"../MusixBox/AudioStreamPlayer3D".stop()
+		$"../Cams/InstallingLinux/VideoPlayer".play()
+		get_node(mon1).set_surface_material(0, texture_linuxInstall)
+		get_node(mon2).set_surface_material(0, texture_linuxInstall)
+		get_node(mon3).set_surface_material(0, texture_linuxInstall)
+		get_node(mon4).set_surface_material(0, texture_linuxInstall)
+		get_node(mon5).set_surface_material(0, texture_linuxInstall)
+		get_node(mon6).set_surface_material(0, texture_linuxInstall)
+		get_node(mon7).set_surface_material(0, texture_linuxInstall)
+		get_node(mon8).set_surface_material(0, texture_linuxInstall)
+	if state == "cams2":
+		$"../MusixBox".show()
+		$"../MusixBox/AudioStreamPlayer3D".unit_db = -25
+	else:
+		$"../MusixBox".hide()
+		$"../MusixBox/AudioStreamPlayer3D".unit_db = -40
+	if state == "back":
+		$"../Backs".show()
+	else:
+		$"../Backs".hide()
+	#debug
+	if Input.is_action_pressed("debug_on"):
+		$"../Control".show()
+	else:
+		$"../Control".hide()
+	$"../Control/AspectRatioContainer/Label".text = "Algert: " + $"../Альберт".position
 
+
+func _on_ResetBut_pressed():
+	flag = false
+	get_node(mon1).set_surface_material(0, texture_camRoom1)
+	get_node(mon2).set_surface_material(0, texture_subShield1)
+	get_node(mon3).set_surface_material(0, texture_subShield2)
+	get_node(mon4).set_surface_material(0, texture_subShield1)
+	get_node(mon5).set_surface_material(0, texture_subShield3)
+	get_node(mon6).set_surface_material(0, texture_subShield3)
+	get_node(mon7).set_surface_material(0, texture_subShieldZayavka)
+	get_node(mon8).set_surface_material(0, texture_camCorridor4)
+	$"../MusixBox/AudioStreamPlayer3D".play()
+	$"../Cams/InstallingLinux/VideoPlayer".stop()
+
+
+func _on_Monetization_pressed():
+	pass # Replace with function body.
